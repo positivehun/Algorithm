7ff87e6b0ae2fcc49f505fe94584af57664191d6
positivehun <jihun3104@gmail.com>
2022-06-15 00:03:30 +0900

    220615

--- 220614/18258.py
+++ 220614/18258.py
@@ -1,13 +1,14 @@
 import sys
 sys.stdin = open("input_18258.txt","r")
 
+#import sys
 from collections import deque
+#input = sys.stdin.readline
 queue = deque()
 N = int(input())
 
 for i in range(0,N):
-    INPUT = input()
-    INPUT = INPUT.split(" ")
+    INPUT = input().split()
     ORDER  = INPUT[0]
     if ORDER == "push":
         queue.append(INPUT[1])
@@ -20,7 +21,8 @@ for i in range(0,N):
         if len(queue) !=0:
             a = queue[0]
             queue.popleft()
-        else:
+            print(a)
+        elif(len(queue)==0):
             print(-1)
     elif ORDER == "size":
         print(len(queue))
