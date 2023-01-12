8a9e4dd82b66cbafe7fcf2fa054fc6d3653283c7
positivehun <jihun3104@gmail.com>
2023-01-12 14:32:02 +0900

    230112

--- 230109/test.c
+++ 230109/test.c
@@ -1,5 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
 int main(void){
-  prinf("Hello world");
+  int a;
+  printf("입력하세요 : ");
+  scanf("%d", &a);
+  printf("%d", a);
+  return 0;
+  // else{
+  //   printf("A는 10 아닙니다");
+  // }
 }
--- 230109/test.cpp
+++ 230109/test.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
-
-int main(void){
-
-  cout >> "Hello" >> 2 >> "hi";
+#include <iostream> //stdio.h printf scanf 
+using namespace std; // std::cout
+int main(void)
+{
+  int N = 10;
+  int score;
+  for (int i = 0; i < N; i++)
+  {
+    cin >> score;
+    cout << "입력받은 점수는 "<<score<<"점 입니다.";
+  }
 }
